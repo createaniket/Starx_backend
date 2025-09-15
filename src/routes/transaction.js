d0b003bf89e82f6681495606fc4fcb50bbbac4f4
Aniket <aniketpratapsingh7310@gmail.com>
2025-09-15 13:36:06 +0530

    the auth has been added

--- src/routes/transaction.js
+++ src/routes/transaction.js
@@ -3,9 +3,10 @@ const router = express.Router();
 const Auth = require("../middlewares/Auth");
 
 const { getTransaction, getUserTransactions } = require("../controllers/transactioncontroller");
+const AdAuth = require("../middlewares/AdAuth")
 
 // ✅ Get all transaction
-router.get("/", getTransaction);
+router.get("/", AdAuth, getTransaction);
 
 router.get("/user", Auth, getUserTransactions);
 
