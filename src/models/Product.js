eff1a8989306fe55efe5b9caa6a703c59416735e
Aniket <aniketpratapsingh7310@gmail.com>
2025-09-21 01:33:32 +0530

    some enhancements

--- src/models/Product.js
+++ src/models/Product.js
@@ -4,8 +4,6 @@ const ProductSchema = new mongoose.Schema({
   name: String,
   description: String,
   imageUrl: String,   // Cloudinary URL
-  qrCount: { type: Number, default: 0 },
-  qrUsed: { type: Number, default: 0 }
 }, { timestamps: true });
 
 module.exports = mongoose.model("Product", ProductSchema);
